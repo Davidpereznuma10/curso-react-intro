4f22ad10fecc8624ed698578aa4d4b985d2f26e6
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-09-14 22:09:38 -0500

    feat 🔥  Remove files
    - This files are remove 'cause there are in their respective directory to manegmen the clean architecture.

--- src/TodoCounter.js
+++
@@ -1,9 +0,0 @@
-import './TodoCounter.css'
-function TodoCounter({ completed, total }){
-    return(
-      <h1 className="TodoCounter">
-        You already has finiched <span>{completed}</span> de <span>{total}</span> ToDos
-      </h1>
-    )
-  };
-  export { TodoCounter }
